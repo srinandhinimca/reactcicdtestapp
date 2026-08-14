425f82cb1ef69d95e5b2ed413417477cfe13c18f
srinandhinimca <annammca07@gmail.com>
2026-08-14 15:43:02 +0530

    Initial React CICD application

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>CI/CD React Application</h1>
+
+      <p>
+        React + Docker + GitHub Actions + Kubernetes
+      </p>
+
+      <h2>Version 1.0</h2>
+    </div>
+  )
+}
+
+export default App
